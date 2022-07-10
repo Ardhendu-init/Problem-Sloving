0a29161f2478b1d17be5b9fa101849239f303afd
Ardhendu Pramanik <68387952+Ardhendu-init@users.noreply.github.com>
2022-07-10 13:33:02 +0530

    Time: 60 ms (47.42%), Space: 113.8 MB (34.67%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -14,5 +14,19 @@ class Solution {
         }
         return nums[nums.length - 1];
     }
-    
 }
+//      public int maxResult(int[] nums, int k) {
+//         int dp[] = new int [nums.length];
+//          dp[nums.length-1] = nums[nums.length-1];
+//          for(int i = nums.length-2 ; i>=0 ; i--){
+//              int max = Integer.MIN_VALUE;  
+//              for(int j = 1 ; j <= k && j+i<nums.length ;j++){
+//                  max = Math.max(max, nums[i]+dp[i+j]);
+//              }
+//              dp[i] = max;
+             
+//          }
+//          return dp[0];
+//     }
+// }
+// https://leetcode.com/problems/jump-game-vi/discuss/2257293/JAVA-or-RECURSIVE-or-MEMOIZED-or-TABULATED-or-OPTIMIZED-or-DP-or
