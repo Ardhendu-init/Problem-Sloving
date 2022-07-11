6c7faa736e3c9d139c7512f80f6b2b26097331fe
Ardhendu Pramanik <68387952+Ardhendu-init@users.noreply.github.com>
2022-07-11 23:02:56 +0530

    Time: 1 ms (95.06%), Space: 40.9 MB (91.84%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,28 +14,26 @@
  * }
  */
 class Solution {
-
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+        List<Integer>ans = new ArrayList<>();
         if(root == null){
             return ans ;
         }
-        Queue<TreeNode> q = new LinkedList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
         q.add(root);
-
-        while (q.size()>0) {
+        while(q.size()>0){
             ans.add(q.peek().val);
             int size = q.size();
-            while (size-- > 0) {
+            while(size --> 0){
                 TreeNode rem = q.remove();
-                if (rem.right != null) {
+                if(rem.right != null){
                     q.add(rem.right);
                 }
-                if (rem.left != null) {
+                if(rem.left != null){
                     q.add(rem.left);
                 }
             }
         }
-        return ans;
+        return ans ;
     }
 }
