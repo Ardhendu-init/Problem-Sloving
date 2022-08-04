7fdf84f078d6da60fe91f31daabec978e183f28c
Ardhendu Pramanik <68387952+Ardhendu-init@users.noreply.github.com>
2022-08-04 21:34:37 +0530

    Time: 3 ms (62.77%), Space: 42.1 MB (42.60%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,24 +1,36 @@
 class Solution {
-
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-        HashMap<Character, Character> dict = new HashMap<Character, Character>();
-        dict.put(')', '(');
-        dict.put('}', '{');
-        dict.put(']', '[');
-        
-        
-        for(int i = 0 ; i < s.length(); i++){
+        Stack<Character>st = new Stack<>();
+        for(int i = 0 ; i < s.length();i++){
             char ch = s.charAt(i);
-            if(dict.containsKey(ch)){
-                char topEle = st.size()==0?'#':st.pop();
-                if(topEle != dict.get(ch)){
-                    return false;
-                }
-            }else{
+            if(ch == '('||ch=='{'|| ch =='['){
                 st.push(ch);
+            }else if(ch ==')' && st.size()>0){
+                if(st.peek()=='('){
+                    st.pop();
+                }else{
+                    return false ;
+                }
+            }else if(ch =='}' && st.size()>0){
+                if(st.peek()=='{'){
+                    st.pop();
+                }else{
+                    return false ;
+                }
+            }else if(ch ==']' && st.size()>0){
+                if(st.peek()=='['){
+                    st.pop();
+                }else{
+                    return false ;
+                }
+            }else {
+                return false ;
             }
         }
-        return st.isEmpty();
+        if(st.size()>0){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
