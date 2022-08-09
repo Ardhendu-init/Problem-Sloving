0bbd1d87c0a03db138d54c6ddddf5deffe178191
Ardhendu Pramanik <68387952+Ardhendu-init@users.noreply.github.com>
2022-08-09 10:57:23 +0530

    Time: 15 ms (84.17%), Space: 47.1 MB (9.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,46 +1,18 @@
 class Solution {
-
-    //Tabulation way : top-down approach
     public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text2.length() + 1][text1.length() + 1];
-
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                char c1 = text1.charAt(j-1);
-                char c2 = text2.charAt(i-1);
-                if (c1 == c2) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        int dp [][] = new int [text1.length()+1][text2.length()+1];
+        
+        for(int i = 1 ; i < dp.length ; i++){
+            for(int j = 1 ; j < dp[0].length ; j++){
+                char ch = text1.charAt(i-1);
+                char ch2 = text2.charAt(j-1);
+                if(ch == ch2){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max (dp[i][j-1], dp[i-1][j]);
                 }
             }
         }
-        return dp[dp.length-1][dp[0].length-1];
+        return dp[text1.length()][text2.length()];
     }
-    //with Memoization
-
-    //     public int longestCommonSubsequence(String text1, String text2) {
-    //         int dp[][] = new int[text1.length() + 1][text2.length() + 1];
-    //         for (int i = 0; i < dp.length; i++) {
-    //             for (int j = 0; j < dp[0].length; j++) {
-    //                 dp[i][j] = -1;
-    //             }
-    //         }
-    //         return lcs(text1, text2, text1.length(), text2.length(), dp);
-    //     }
-
-    //     public static int lcs(String s1, String s2, int m, int n, int dp[][]) {
-    //         if (m == 0 || n == 0) {
-    //             return 0;
-    //         }
-    //         if (dp[m][n] != -1) {
-    //             return dp[m][n];
-    //         }
-    //         if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
-    //             dp[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, dp);
-    //             return dp[m][n];
-    //         } else {
-    //             return dp[m][n] = Math.max(lcs(s1, s2, m, n - 1, dp), lcs(s1, s2, m - 1, n, dp));
-    //         }
-    //     }
 }
