4478d4cc011d9ef214707ddc8ef0f291d4296a93
Ardhendu Pramanik <68387952+Ardhendu-init@users.noreply.github.com>
2022-08-10 11:46:32 +0530

    Time: 40 ms (24.33%), Space: 48.5 MB (39.79%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -3,15 +3,17 @@ class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int m = str1.length();
         int n = str2.length();
-        if (m == 0 || n == 0) {
+        if(m == 0 || n== 0){
             return "";
         }
         int dp[][] = new int[m + 1][n + 1];
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                char c1 = str1.charAt(i - 1);
+                char c2 = str2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
@@ -21,21 +23,23 @@ class Solution {
         int j = n;
         String res = "";
         while (i > 0 && j > 0) {
-            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                res = str1.charAt(i - 1) + res;
+            char c1 = str1.charAt(i - 1);
+            char c2 = str2.charAt(j - 1);
+            if (c1 == c2) {
+                res = c2+res;
                 i--;
                 j--;
             } else {
                 if (dp[i - 1][j] > dp[i][j - 1]) {
-                    res = str1.charAt(i - 1) + res;
+                    res = c1+res;
                     i--;
                 } else {
-                    res = str2.charAt(j - 1) + res;
+                    res = c2+res;
                     j--;
                 }
             }
         }
-        if( j != 0){
+           if( j != 0){
             String s = str2.substring(0,j);
             res = s+res;
         }
@@ -43,6 +47,7 @@ class Solution {
             String s = str1.substring(0,i);
             res = s+res;
         }
+
         return res;
     }
 }
